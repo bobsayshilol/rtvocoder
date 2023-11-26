d2d6f298560510b30ebc1931a71348bfb5c3147a
bobsayshilol <bobsayshilol@live.co.uk>
2023-11-26 14:16:00 +0000

    Enable warnings as errors

--- lib/BandPass.cc
+++ lib/BandPass.cc
@@ -10,10 +10,14 @@ BandPass::~BandPass() {}
 
 void BandPass::reset(float sampling_rate, float hz, float q) {
     // TODO
+    (void)sampling_rate;
+    (void)hz;
+    (void)q;
 }
 
 void BandPass::process(std::span<float> input) {
     // TODO
+    (void)input;
 }
 
 }  // namespace pwv
